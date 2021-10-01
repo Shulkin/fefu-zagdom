71e78d8b0e6c08de4bf40f59d08842592c7892ec
Evgeny Shulkin <shlk@feip.co>
2021-10-02 02:14:37 +1000

    Lesson 3: Create site layout

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,9 @@
 const mix = require('laravel-mix');
 
 mix
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css')
+    .setPublicPath('public')
+    .js('resources/scripts/main.js', 'build/bundle.js')
+    .sass('resources/styles/main.scss', 'build/bundle.css')
     .browserSync({
         proxy: 'localhost',
         files: ['resources/**/*'],
