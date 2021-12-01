4b887e93874d6ffd6880b1231cfadfc31e7adc36
Evgeny Shulkin <shlk@feip.co>
2021-12-01 15:52:49 +1000

    Lesson 10: Tweak map settings

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -6,8 +6,18 @@ document.addEventListener('MapLoaded', () => {
         ymaps.ready(() => {
             const map = new ymaps.Map('map', {
                 center: [43.159, 131.945],
+                controls: ['geolocationControl', 'zoomControl', 'fullscreenControl'],
                 zoom: 11,
             });
+
+            const placemark = new ymaps.Placemark(map.getCenter(), {}, {
+                iconLayout: 'default#image',
+                iconImageHref: 'assets/icons/map-marker-green.svg',
+                iconImageSize: [42, 56],
+                iconImageOffset: [-21, -56],
+            });
+
+            map.geoObjects.add(placemark);
         });
     }
 });
