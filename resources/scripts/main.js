8d2b949c48a680c51d9fe4f4b167ffe29168d413
Evgeny Shulkin <shlk@feip.co>
2021-12-01 13:53:16 +1000

    Lesson 10: Add map to contacts page

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,2 +1,13 @@
 import './uikit';
 import './swiper';
+
+document.addEventListener('MapLoaded', () => {
+    if (window.ymaps !== undefined) {
+        ymaps.ready(() => {
+            const map = new ymaps.Map('map', {
+                center: [43.159, 131.945],
+                zoom: 11,
+            });
+        });
+    }
+});
