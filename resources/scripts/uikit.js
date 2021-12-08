ec84704652ef57b41f1ba476aa23140bd337a0bc
doomedboi <73707198+doomedboi@users.noreply.github.com>
2021-12-08 13:56:44 +1000

    finished the header
    finish header, added resources, postcss-axis plugin

---
+++ resources/scripts/uikit.js
@@ -0,0 +1,6 @@
+import UIkit from "uikit/dist/js/uikit-core";
+
+import Lightbox from 'uikit/dist/js/components/lightbox';
+UIkit.component('lightbox', Lightbox);
+
+export default UIkit;
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,10 @@ const mix = require('laravel-mix');
 mix
     .setPublicPath('public')
     .js('resources/scripts/main.js', 'build/bundle.js')
-    .sass('resources/styles/main.scss', 'build/bundle.css')
+    .sass('resources/styles/main.scss', 'build/bundle.css', {}, [
+        require('postcss-axis')
+    ]
+    )
     .browserSync({
         watch: true,
         proxy: 'localhost',
