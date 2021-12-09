4215b6024a1681fc13fadaee369410d7a58c207f
doomedboi <73707198+doomedboi@users.noreply.github.com>
2021-12-10 01:54:47 +1000

    5 & 6 dz
    done.

---
+++ resources/scripts/swiper.js
@@ -0,0 +1,21 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('[data-swiper]', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
