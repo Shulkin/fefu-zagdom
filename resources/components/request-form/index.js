34d38b8e3bfbaee6ade8d69929311979f9587e20
Evgeny Shulkin <shlk@feip.co>
2021-12-23 12:41:37 +1000

    Lesson 13: Send POST request

--- resources/components/request-form/index.js
+++ resources/components/request-form/index.js
@@ -7,6 +7,12 @@ const formDataSchema = () => ({
     message: '',
 });
 
+const createPayload = (data) => ({
+    name: data.name,
+    phone: data.phone,
+    message: data.message,
+})
+
 export default (el, name) =>
     new Vue({
         el,
@@ -14,8 +20,31 @@ export default (el, name) =>
         directives: { mask },
         data: () => ({
             data: formDataSchema(),
+            isLoading: false,
         }),
         methods: {
+            async submit() {
+                if (this.isLoading) return;
+
+                const payload = createPayload(this.data);
+
+                try {
+                    const response = await fetch('/api/request', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+                        body: JSON.stringify(payload),
+                    });
+
+                    const result = await response.json();
+
+                    alert('Ваша заявка успешно отправлена');
+                } catch (error) {
+                    console.warn(error);
+                } finally {
+                    this.isLoading = false;
+                }
+            },
+
             onInputPhone(event) {
                 if (/^[8]/.test(event.currentTarget.value)) {
                     event.currentTarget.value = event.currentTarget.value.replace(/^[8]/g, '+7');
